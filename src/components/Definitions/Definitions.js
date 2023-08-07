7cf5601946093d760d60cda808c07daf094d9bf2
Priyanka Chakraborty <chakraborty.priyanka@gmail.com>
2023-08-07 20:25:50 +0530

    additional changes

--- src/components/Definitions/Definitions.js
+++ src/components/Definitions/Definitions.js
@@ -1,55 +1,51 @@
-import React from 'react'
-import './Definitions.css'
+import React from "react";
+import "./Definitions.css";
 
-const Definitions = ({word,meanings,category}) => {
+const Definitions = ({ meanings, word, LightTheme, category }) => {
   return (
-    <div className='meanings'>
-    {meanings[0] && word && category === 'en' &&(
-      <audio
-      src={meanings[0].phonetics[0] && meanings[0].phonetics[0].audio}
-      style={{backgroundColor:'#fff',borderRadius:10}}
-      controls
-      >
+    <div className="meanings">
+      {meanings[0] && word && category === "en" && (
+        <audio
+          style={{ backgroundColor: "#fff", borderRadius: 10 }}
+          src={meanings[0].phonetics[0] && meanings[0].phonetics[0].audio}
+          controls
+        >
+          Your browser does not support the audio element.
+        </audio>
+      )}
 
-      </audio>
-    )}
-    
-    {
-      word === "" ? (
-        <span className='subTitle'>Start by typing</span>
+      {word === "" ? (
+        <span className="subTitle">Start by typing a word in search</span>
       ) : (
-        
-        meanings.map((mean)=>
-          mean.meanings.map((item)=>
-          item.definitions.map((def)=>
-          <div
-          className='singleMean'
-          style={{
-            backgroundColor:"white",
-            color:"black"
-            }}
-            >
-            <b>{def.definition}</b>
-            <hr style={{backgroundColor:"black",width:"100%"}} />
-            {
-              def.example && (
-                <span>
-                  <b>Example:</b>
-                  {def.example}
-                </span>
-              )
-            }
-            {def.synonyms && <span>
-                  <b>Synonyms:</b>
-                  {def.synonyms.map((synonym)=> `${synonym}, `)}
-                </span>}
-          </div>
-          )
+        meanings.map((mean) =>
+          mean.meanings.map((item) =>
+            item.definitions.map((def) => (
+              <div
+                className="singleMean"
+                style={{
+                  backgroundColor: LightTheme ? "#3b5360" : "white",
+                  color: LightTheme ? "white" : "black",
+                }}
+              >
+                <b>{def.definition}</b>
+                <hr style={{ backgroundColor: "black", width: "100%" }} />
+                {def.example && (
+                  <span>
+                    <b>Example :</b> {def.example}
+                  </span>
+                )}
+                {def.synonyms && (
+                  <span>
+                    <b>Synonyms :</b> {def.synonyms.map((s) => `${s}, `)}
+                  </span>
+                )}
+              </div>
+            ))
           )
         )
-      )
-    }</div>
-  )
-}
+      )}
+    </div>
+  );
+};
 
-export default Definitions
+export default Definitions;
